c51d52768348e5443ed538dd10e40cdcef62193e
KungeniDjungeln <thhe05001@utb.vaxjo.se>
2022-10-05 13:19:35 +0200

    Add project files.

---
+++ Uppgift 3.14 v2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace Uppgift_3_14_v2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Har du klarat av gymnasiet?");
+            string svar = Console.ReadLine().ToLower();
+
+
+            if (svar == "j")
+            {
+                Console.WriteLine("Hur gammal är du?");
+                int ålder = int.Parse(Console.ReadLine());
+
+            if (ålder <= 22 && ålder >= 18)
+                {
+                    Console.WriteLine("Vi vill anställa dig");
+
+                }
+
+                else
+                {
+                    Console.WriteLine("Vi är tyvärr inte intresserade");
+                }
+                
+
+            }
+
+            else if (svar == "n")
+            {
+                Console.WriteLine("Nej tack");
+            }
+        }
+    }
+}
